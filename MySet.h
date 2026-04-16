c4f024e5f3e31e572217fcf838b4dbdbcf165f8a
Nishantshah07 <139062418+Nishantshah07@users.noreply.github.com>
2026-04-16 14:27:42 -0400

    Add files via upload

---
+++ MySet.h
@@ -0,0 +1,45 @@
+#ifndef _MYSET_H_
+#define _MYSET_H_
+
+#include <iostream>
+#include <stdexcept>
+#include "SetInterface.h"
+
+namespace csce240h_project3 {
+
+template <class T>
+class MySet : public SetInterface<T> {
+ public:
+  // constructors / destructor
+  MySet();
+  MySet(const MySet<T>& other);
+  MySet<T>& operator=(const MySet<T>& other);
+  ~MySet();
+
+  // required interface functions
+  void Print() const override;
+  bool IsElementOf(T value) const override;
+  int Cardinality() const override;
+  bool AddElement(const T& value) override;
+  bool RemoveElement(const T& value) override;
+  T GetElement(int index) const override;
+
+  bool IsSubsetOf(const SetInterface<T>& superset) const override;
+  bool IsSupersetOf(const SetInterface<T>& subset) const override;
+
+  void Intersection(const SetInterface<T>& set2) override;
+  void Union(const SetInterface<T>& set2) override;
+  void Difference(const SetInterface<T>& set2) override;
+
+ private:
+  T* elements_;
+  int size_;
+  int capacity_;
+
+  void Resize();
+  int IndexOf(const T& value) const;
+};
+
+}  // namespace csce240h_project3
+
+#endif  // _MYSET_H_
