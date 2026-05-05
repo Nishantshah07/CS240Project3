1d23b0084f82c54c2116a8311e821e758ad6e79d
Braden Guliano <72895782+bguliano@users.noreply.github.com>
2026-05-05 01:53:14 -0400

    add comments to demo.cc

--- demo.cc
+++ demo.cc
@@ -4,6 +4,9 @@
 #include "MySet.h"
 
 using csce240h_project3::MySet;
+using std::cin;
+using std::cout;
+using std::endl;
 
 // Converts one character to lowercase without using extra libraries
 char ToLowerChar(char c) {
@@ -29,9 +32,9 @@ bool EqualsIgnoreCase(const char first[], const char second[]) {
 template <class T>
 void PrintAllSets(MySet<T> sets[], int number_of_sets) {
   for (int i = 0; i < number_of_sets; ++i) {
-    std::cout << "Set" << i + 1 << ": ";
+    cout << "Set" << i + 1 << ": ";
     sets[i].Print();
-    std::cout << std::endl;
+    cout << endl;
   }
 }
 
@@ -39,14 +42,14 @@ void PrintAllSets(MySet<T> sets[], int number_of_sets) {
 int GetSetNumber(int number_of_sets) {
   int set_number;
 
-  std::cout << "Select a set by number ";
-  std::cout << "(1 = Set1, 2 = Set2, etc.): ";
-  std::cin >> set_number;
+  cout << "Select a set by number ";
+  cout << "(1 = Set1, 2 = Set2, etc.): ";
+  cin >> set_number;
 
   while (set_number < 1 || set_number > number_of_sets) {
-    std::cout << "Invalid set number. Enter a number from 1 to ";
-    std::cout << number_of_sets << ": ";
-    std::cin >> set_number;
+    cout << "Invalid set number. Enter a number from 1 to ";
+    cout << number_of_sets << ": ";
+    cin >> set_number;
   }
 
   return set_number;
@@ -55,230 +58,291 @@ int GetSetNumber(int number_of_sets) {
 // Runs the demo for either int or double sets
 template <class T>
 void RunDemo() {
+  // set demo parameters
+  const int kMinSets = 2;
   const int kMaxSets = 5;
+  const int kMinValues = 0;
   const int kMaxValues = 10;
 
   MySet<T> sets[kMaxSets];
 
   int number_of_sets;
 
-  std::cout << "How many sets do you want to create? ";
-  std::cout << "(minimum 2, maximum 5): ";
-  std::cin >> number_of_sets;
+  // gets the number of sets and input validation
+  cout << "How many sets do you want to create? ";
+  cout << "(minimum " << kMinSets << ", maximum " << kMaxSets << "): ";
+  cin >> number_of_sets;
 
-  while (number_of_sets < 2 || number_of_sets > 5) {
-    std::cout << "Invalid number of sets. Enter a number from 2 to 5: ";
-    std::cin >> number_of_sets;
+  // ensure input is correct before continuing
+  while (number_of_sets < kMinSets || number_of_sets > kMaxSets) {
+    cout << "Invalid number of sets. Enter a number from ";
+    cout << kMinSets << " to " << kMaxSets << ": ";
+    cin >> number_of_sets;
   }
 
+  // fill each set requested
   for (int i = 0; i < number_of_sets; ++i) {
     int set_size;
 
-    std::cout << "Enter size of Set" << i + 1 << ": ";
-    std::cin >> set_size;
+    // gets each starting set and demos AddElement rejecting duplicates
+    cout << "Enter size of Set" << i + 1 << ": ";
+    cin >> set_size;
 
-    while (set_size < 0 || set_size > kMaxValues) {
-      std::cout << "Invalid size. Enter a size from 0 to 10: ";
-      std::cin >> set_size;
+    while (set_size < kMinValues || set_size > kMaxValues) {
+      cout << "Invalid size. Enter a size from ";
+      cout << kMinValues << " to " << kMaxValues << ": ";
+      cin >> set_size;
     }
 
-    std::cout << "Enter values for Set" << i + 1 << ": ";
+    cout << "Enter values for Set" << i + 1 << ": ";
 
     for (int j = 0; j < set_size; ++j) {
       T value;
-      std::cin >> value;
+      cin >> value;
       sets[i].AddElement(value);
     }
   }
 
-  std::cout << std::endl;
-  std::cout << "Created sets:" << std::endl;
+  cout << endl;
+  cout << "Created sets:" << endl;
   PrintAllSets(sets, number_of_sets);
 
+  // certain commands can be executed in demo
   char command[50];
   bool quit = false;
 
+  // loop until user specifies quit
   while (!quit) {
-    std::cout << std::endl;
+    cout << endl;
 
     int selected_set_number = GetSetNumber(number_of_sets);
     int selected_index = selected_set_number - 1;
 
-    std::cout << std::endl;
-    std::cout << "Available methods:" << std::endl;
-    std::cout << "IsElementOf" << std::endl;
-    std::cout << "Cardinality" << std::endl;
-    std::cout << "AddElement" << std::endl;
-    std::cout << "RemoveElement" << std::endl;
-    std::cout << "GetElement" << std::endl;
-    std::cout << "IsSubsetOf" << std::endl;
-    std::cout << "IsSupersetOf" << std::endl;
-    std::cout << "Intersection" << std::endl;
-    std::cout << "Union" << std::endl;
-    std::cout << "Difference" << std::endl;
-    std::cout << "Quit" << std::endl;
+    // shows the required methods available from SetInterface
+    cout << endl;
+    cout << "Available methods:" << endl;
+    cout << "IsElementOf" << endl;
+    cout << "Cardinality" << endl;
+    cout << "AddElement" << endl;
+    cout << "RemoveElement" << endl;
+    cout << "GetElement" << endl;
+    cout << "IsSubsetOf" << endl;
+    cout << "IsSupersetOf" << endl;
+    cout << "Intersection" << endl;
+    cout << "Union" << endl;
+    cout << "Difference" << endl;
+    cout << "Quit" << endl;
 
-    std::cout << "Enter method name: ";
-    std::cin >> command;
+    cout << "Enter method name: ";
+    cin >> command;
 
+    // essentially a large if-else chain checking for each command
     if (EqualsIgnoreCase(command, "quit")) {
       quit = true;
-    } else if (EqualsIgnoreCase(command, "iselementof")) {
+    }
+
+    // IsElementOf
+    // checks whether one value is in the selected set
+    else if (EqualsIgnoreCase(command, "iselementof")) {
       T value;
 
-      std::cout << "Enter value to check: ";
-      std::cin >> value;
+      cout << "Enter value to check: ";
+      cin >> value;
 
       if (sets[selected_index].IsElementOf(value))
-        std::cout << value << " is an element of Set";
+        cout << value << " is an element of Set";
       else
-        std::cout << value << " is not an element of Set";
+        cout << value << " is not an element of Set";
 
-      std::cout << selected_set_number << "." << std::endl;
+      cout << selected_set_number << "." << endl;
 
-    } else if (EqualsIgnoreCase(command, "cardinality")) {
-      std::cout << "Cardinality of Set" << selected_set_number << ": ";
-      std::cout << sets[selected_index].Cardinality() << std::endl;
+    }
 
-    } else if (EqualsIgnoreCase(command, "addelement")) {
+    // Cardinality
+    // gets the number of unique values in the set
+    else if (EqualsIgnoreCase(command, "cardinality")) {
+      cout << "Cardinality of Set" << selected_set_number << ": ";
+      cout << sets[selected_index].Cardinality() << endl;
+
+    }
+
+    // AddElement
+    // adds a new value or rejecting a duplicate value
+    else if (EqualsIgnoreCase(command, "addelement")) {
       T value;
 
-      std::cout << "Enter value to add: ";
-      std::cin >> value;
+      cout << "Enter value to add: ";
+      cin >> value;
 
       if (sets[selected_index].AddElement(value))
-        std::cout << value << " was added." << std::endl;
+        cout << value << " was added." << endl;
       else
-        std::cout << value << " was already in the set." << std::endl;
+        cout << value << " was already in the set." << endl;
 
-    } else if (EqualsIgnoreCase(command, "removeelement")) {
+    }
+
+    // RemoveElement
+    // removes a value or leaves the set unchanged
+    else if (EqualsIgnoreCase(command, "removeelement")) {
       T value;
 
-      std::cout << "Enter value to remove: ";
-      std::cin >> value;
+      cout << "Enter value to remove: ";
+      cin >> value;
 
       if (sets[selected_index].RemoveElement(value))
-        std::cout << value << " was removed." << std::endl;
+        cout << value << " was removed." << endl;
       else
-        std::cout << value << " was not in the set." << std::endl;
+        cout << value << " was not in the set." << endl;
 
-    } else if (EqualsIgnoreCase(command, "getelement")) {
+    }
+
+    // GetElement
+    // retrieves an element by its array index
+    else if (EqualsIgnoreCase(command, "getelement")) {
       int index;
 
-      std::cout << "Enter index: ";
-      std::cin >> index;
+      cout << "Enter index: ";
+      cin >> index;
 
-      std::cout << "Element at index " << index << ": ";
-      std::cout << sets[selected_index].GetElement(index) << std::endl;
+      cout << "Element at index " << index << ": ";
+      cout << sets[selected_index].GetElement(index) << endl;
 
-    } else if (EqualsIgnoreCase(command, "issubsetof")) {
+    }
+
+    // IsSubsetOf
+    // compares the selected set to another possible superset
+    else if (EqualsIgnoreCase(command, "issubsetof")) {
       int other_set_number = GetSetNumber(number_of_sets);
       int other_index = other_set_number - 1;
 
       if (sets[selected_index].IsSubsetOf(sets[other_index])) {
-        std::cout << "Set" << selected_set_number;
-        std::cout << " is a subset of Set" << other_set_number << ".";
-        std::cout << std::endl;
+        cout << "Set" << selected_set_number;
+        cout << " is a subset of Set" << other_set_number << ".";
+        cout << endl;
       } else {
-        std::cout << "Set" << selected_set_number;
-        std::cout << " is not a subset of Set" << other_set_number << ".";
-        std::cout << std::endl;
+        cout << "Set" << selected_set_number;
+        cout << " is not a subset of Set" << other_set_number << ".";
+        cout << endl;
       }
 
-    } else if (EqualsIgnoreCase(command, "issupersetof")) {
+    }
+
+    // IsSupersetOf
+    // compares the selected set to another possible subset
+    else if (EqualsIgnoreCase(command, "issupersetof")) {
       int other_set_number = GetSetNumber(number_of_sets);
       int other_index = other_set_number - 1;
 
       if (sets[selected_index].IsSupersetOf(sets[other_index])) {
-        std::cout << "Set" << selected_set_number;
-        std::cout << " is a superset of Set" << other_set_number << ".";
-        std::cout << std::endl;
+        cout << "Set" << selected_set_number;
+        cout << " is a superset of Set" << other_set_number << ".";
+        cout << endl;
       } else {
-        std::cout << "Set" << selected_set_number;
-        std::cout << " is not a superset of Set" << other_set_number << ".";
-        std::cout << std::endl;
+        cout << "Set" << selected_set_number;
+        cout << " is not a superset of Set" << other_set_number << ".";
+        cout << endl;
       }
 
-    } else if (EqualsIgnoreCase(command, "intersection")) {
+    }
+
+    // Intersection
+    // changes the set to only values in both sets
+    else if (EqualsIgnoreCase(command, "intersection")) {
       int other_set_number = GetSetNumber(number_of_sets);
       int other_index = other_set_number - 1;
 
       sets[selected_index].Intersection(sets[other_index]);
 
-      std::cout << "Set" << selected_set_number;
-      std::cout << " was changed to its intersection with Set";
-      std::cout << other_set_number << "." << std::endl;
+      cout << "Set" << selected_set_number;
+      cout << " was changed to its intersection with Set";
+      cout << other_set_number << "." << endl;
 
-    } else if (EqualsIgnoreCase(command, "union")) {
+    }
+
+    // Union
+    // changes the set to include values from both sets
+    else if (EqualsIgnoreCase(command, "union")) {
       int other_set_number = GetSetNumber(number_of_sets);
       int other_index = other_set_number - 1;
 
       sets[selected_index].Union(sets[other_index]);
 
-      std::cout << "Set" << selected_set_number;
-      std::cout << " was changed to its union with Set";
-      std::cout << other_set_number << "." << std::endl;
+      cout << "Set" << selected_set_number;
+      cout << " was changed to its union with Set";
+      cout << other_set_number << "." << endl;
 
-    } else if (EqualsIgnoreCase(command, "difference")) {
+    }
+
+    // Difference
+    // removes values that are also in the other set
+    else if (EqualsIgnoreCase(command, "difference")) {
       int other_set_number = GetSetNumber(number_of_sets);
       int other_index = other_set_number - 1;
 
       sets[selected_index].Difference(sets[other_index]);
 
-      std::cout << "Set" << selected_set_number;
-      std::cout << " was changed to its difference with Set";
-      std::cout << other_set_number << "." << std::endl;
+      cout << "Set" << selected_set_number;
+      cout << " was changed to its difference with Set";
+      cout << other_set_number << "." << endl;
 
-    } else {
-      std::cout << "Invalid method name." << std::endl;
     }
 
+    // invalid command
+    else {
+      cout << "Invalid method name." << endl;
+    }
+
+    // shows the result of the method that was just demonstrated
     if (!quit) {
-      std::cout << "Set" << selected_set_number << ": ";
+      cout << "Set" << selected_set_number << ": ";
       sets[selected_index].Print();
-      std::cout << std::endl;
+      cout << endl;
     }
   }
 
-  std::cout << std::endl;
-  std::cout << "Operator demonstrations using Set1 and Set2:" << std::endl;
+  cout << endl;
+  cout << "Additional functionality operator demonstrations using Set1 and Set2:" << endl;
 
+  // demos overloaded operators that return new MySet objects
+  // this is why minimum number of sets is 2
   MySet<T> union_set = sets[0] + sets[1];
   MySet<T> difference_set = sets[0] - sets[1];
   MySet<T> intersection_set = sets[0] * sets[1];
 
-  std::cout << "Set1 + Set2 = ";
+  cout << "Set1 + Set2 = ";
   union_set.Print();
-  std::cout << std::endl;
+  cout << endl;
 
-  std::cout << "Set1 - Set2 = ";
+  cout << "Set1 - Set2 = ";
   difference_set.Print();
-  std::cout << std::endl;
+  cout << endl;
 
-  std::cout << "Set1 * Set2 = ";
+  cout << "Set1 * Set2 = ";
   intersection_set.Print();
-  std::cout << std::endl;
+  cout << endl;
 
-  std::cout << "Set1 == Set2 = ";
+  cout << "Set1 == Set2 = ";
 
   if (sets[0] == sets[1])
-    std::cout << "true";
+    cout << "true";
   else
-    std::cout << "false";
+    cout << "false";
 
-  std::cout << std::endl;
+  cout << endl;
 }
 
 int main() {
   char type_choice[20];
 
-  std::cout << "Enter type of sets, int or double: ";
-  std::cin >> type_choice;
+  // lets the user choose which template type to demonstrate
+  cout << "Enter type of sets, int or double: ";
+  cin >> type_choice;
 
+  // keep asking if invalid input
   while (!EqualsIgnoreCase(type_choice, "int") &&
          !EqualsIgnoreCase(type_choice, "double")) {
-    std::cout << "Invalid type. Enter int or double: ";
-    std::cin >> type_choice;
+    cout << "Invalid type. Enter int or double: ";
+    cin >> type_choice;
   }
 
   if (EqualsIgnoreCase(type_choice, "int"))
